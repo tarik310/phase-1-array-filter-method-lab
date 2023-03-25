bb912583a12a02888a2ce28d2c10233a948ca819
Tareq Harh <tareq310harh@gmail.com>
2023-03-25 12:18:15 +0300

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(drvname => name.toLowerCase() === drvname.toLowerCase())
+}
+function fuzzyMatch(drivers, name){
+    return drivers.filter(drvname =>drvname.startsWith(name))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
